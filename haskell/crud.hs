6e9052652c71dc7e97552e5f618ac5ab021e9d4c
Jhonatan Batista <jhonatanbds@outlook.com>
2019-11-28 00:06:40 -0300

    final

--- haskell/crud.hs
+++ haskell/crud.hs
@@ -21,87 +21,8 @@ instance ToJSON Transaction
 
 getId t = (textoIdentificador t)
 
-getJSON :: IO B.ByteString
-getJSON = B.readFile "data.json"
-
-filterByYear :: Int -> IO()
-filterByYear y = do
-  d <- (eitherDecode <$> getJSON) :: IO (Either String [Transaction])
-  case d of
-    Left err -> putStrLn err
-    Right ps -> print [x | x <- ps, (year (data_ x)) == y ]
-
-filterByYearMonth :: Int -> Int -> IO()
-filterByYearMonth y m= do
-  d <- (eitherDecode <$> getJSON) :: IO (Either String [Transaction])
-  case d of
-    Left err -> putStrLn err
-    Right ps -> print (filterByMonthYear m y ps)
-
-
-getIncome  :: Int -> Int -> IO()
-getIncome m y  = do
-  d <- (eitherDecode <$> getJSON) :: IO (Either String [Transaction])
-  case d of
-    Left err -> putStrLn err
-    Right ps -> print (sum (getAllIncome m y (cleanData ps)))
-
-getExpense  :: Int -> Int -> IO()
-getExpense m y  = do
-  d <- (eitherDecode <$> getJSON) :: IO (Either String [Transaction])
-  case d of
-    Left err -> putStrLn err
-    Right ps -> print (sum (getAllExpense m y (cleanData ps)))
-
-getLeftOver :: Int -> Int -> IO()
-getLeftOver m y = do
-  d <- (eitherDecode <$> getJSON) :: IO (Either String [Transaction])
-  case d of
-    Left err -> putStrLn err
-    Right ps -> print (sum (getAllIncome m y (cleanData ps)) - sum (getAllExpense m y (cleanData ps)) )
-  
-
-getMeanLeftOver :: Int -> IO()
-getMeanLeftOver y = do
-  d <- (eitherDecode <$> getJSON) :: IO (Either String [Transaction])
-  case d of
-    Left err -> putStrLn err
-    Right ps -> print ((sum (getAllIncomeByYear y (cleanData ps)) - sum (getAllExpenseByYear y (cleanData ps)) ) / (fromIntegral (Prelude.length  (getAllExpenseByYear y (cleanData ps)))))
-
-getMaxBalance :: Int -> Int -> IO ()
-getMaxBalance m y = do
-  d <- (eitherDecode <$> getJSON) :: IO (Either String [Transaction])
-  case d of
-    Left err -> putStrLn err
-    Right ps -> print (getMax (filterByMonthYear m y (cleanData ps)))
-
-getMinBalance :: Int -> Int -> IO ()
-getMinBalance m y = do
-  d <- (eitherDecode <$> getJSON) :: IO (Either String [Transaction])
-  case d of
-    Left err -> putStrLn err
-    Right ps -> print (getMin (filterByMonthYear m y (cleanData ps)))
-
-getMeanIncomeByYear :: Int -> IO ()
-getMeanIncomeByYear y = do
-  d <- (eitherDecode <$> getJSON) :: IO (Either String [Transaction])
-  case d of
-    Left err -> putStrLn err
-    Right ps -> print ((sum  (getAllIncomeByYear y (cleanData ps))) / (fromIntegral (Prelude.length  (getAllIncomeByYear y (cleanData ps)))))
-
-getMeanExpenseByYear :: Int -> IO ()
-getMeanExpenseByYear y = do
-  d <- (eitherDecode <$> getJSON) :: IO (Either String [Transaction])
-  case d of
-    Left err -> putStrLn err
-    Right ps -> print ((sum  (getAllExpenseByYear y (cleanData ps))) / (fromIntegral (Prelude.length  (getAllExpenseByYear y (cleanData ps)))))
-
-getFlux :: Int -> Int -> IO ()
-getFlux m y = do
-  d <- (eitherDecode <$> getJSON) :: IO (Either String [Transaction])
-  case d of
-    Left err -> putStrLn err
-    Right ps -> print (flux (filterByMonthYear m y (cleanData ps)))
+flux :: [Transaction] -> [(Int, Float)]
+flux dado = Prelude.map (\x -> (x, dailyBalance dado x)) (daysInMonth dado)
 
 daysInMonth :: [Transaction] -> [Int]
 daysInMonth dado = quickSort (Data.Foldable.foldl (\acc x -> if not (elem ( dayOfMonth (data_ x))  acc) then acc ++ [( dayOfMonth (data_ x)) ] else acc) [ ( dayOfMonth (data_ (Prelude.head dado)))] dado)
@@ -109,20 +30,16 @@ daysInMonth dado = quickSort (Data.Foldable.foldl (\acc x -> if not (elem ( dayO
 dailyBalance :: [Transaction] -> Int -> Float
 dailyBalance dado dia = sum [(valor x) | x <- [x | x <- dado, ( dayOfMonth (data_ x)) == dia]] 
 
-flux :: [Transaction] -> [(Int, Float)]
-flux dado = Prelude.map (\x -> (x, dailyBalance dado x)) (daysInMonth dado)
-
-filterByMonthYear :: Int -> Int -> [Transaction] -> [Transaction]
-filterByMonthYear m y dado = [x | x <- dado, ( year (data_ x)) == y, ( month (data_ x)) == m ]
-
-cleanData :: [Transaction] -> [Transaction]
-cleanData dado = [x | x <- dado, not ("SALDO_CORRENTE" `elem` (tipos x)), not ("APLICACAO" `elem` (tipos x)), not ("VALOR_APLICACAO" `elem` (tipos x))  ]
-
 quickSort :: (Ord a) => [a] -> [a]
 quickSort [] = []
 quickSort (x:xs)  = quickSort smaller ++ [x] ++ quickSort larger
     where smaller = Prelude.filter (<=x) xs
           larger  = Prelude.filter (> x) xs
+filterByMonthYear :: Int -> Int -> [Transaction] -> [Transaction]
+filterByMonthYear m y dado = [x | x <- dado, ( year (data_ x)) == y, ( month (data_ x)) == m ]
+
+cleanData :: [Transaction] -> [Transaction]
+cleanData dado = [x | x <- dado, not ("SALDO_CORRENTE" `elem` (tipos x)), not ("APLICACAO" `elem` (tipos x)), not ("VALOR_APLICACAO" `elem` (tipos x))  ]
 
 getMin :: [Transaction] -> Transaction
 getMin dado = getMin' (Prelude.head dado) (Prelude.tail dado)
@@ -151,129 +68,208 @@ getAllExpense m y dado = [(valor x) | x <- dado, ( year (data_ x)) == y, ( month
 getAllIncomeByYear y dado = [(valor x) | x <- dado, ( year (data_ x)) == y, ( valor x) > 0 ]
 getAllExpenseByYear y dado =  [(valor x) | x <- dado, ( year (data_ x)) == y, ( valor x) < 0 ]
 
+getJSON :: IO B.ByteString
+getJSON = B.readFile "data.json"
+
+filterByYear :: Int -> IO()
+filterByYear y = do
+  json <- (eitherDecode <$> getJSON) :: IO (Either String [Transaction])
+  case json of
+    Left err -> putStrLn err
+    Right d -> print (filterByYear_ d y)
+
+filterByYear_ d y = [x | x <- d, (year (data_ x)) == y ]
+
+filterByYearMonth :: Int -> Int -> IO()
+filterByYearMonth y m= do
+  json <- (eitherDecode <$> getJSON) :: IO (Either String [Transaction])
+  case json of
+    Left err -> putStrLn err
+    Right d -> print (filterByMonthYear m y d)
+
+
+getIncome  :: Int -> Int -> IO()
+getIncome m y  = do
+  json <- (eitherDecode <$> getJSON) :: IO (Either String [Transaction])
+  case json of
+    Left err -> putStrLn err
+    Right d -> print (sum (getAllIncome m y (cleanData d)))
+
+getExpense  :: Int -> Int -> IO()
+getExpense m y  = do
+  json <- (eitherDecode <$> getJSON) :: IO (Either String [Transaction])
+  case json of
+    Left err -> putStrLn err
+    Right d -> print (sum (getAllExpense m y (cleanData d)))
+
+getLeftOver :: Int -> Int -> IO()
+getLeftOver m y = do
+  json <- (eitherDecode <$> getJSON) :: IO (Either String [Transaction])
+  case json of
+    Left err -> putStrLn err
+    Right d -> print (getLeftOver_ d m y)
+
+getLeftOver_ d m y = sum (getAllIncome m y (cleanData d)) - sum (getAllExpense m y (cleanData d)) 
+  
+
+getMeanLeftOver :: Int -> IO()
+getMeanLeftOver y = do
+  json <- (eitherDecode <$> getJSON) :: IO (Either String [Transaction])
+  case json of
+    Left err -> putStrLn err
+    Right d -> print ( getMeanLeftOver_ d y )
+
+getMeanLeftOver_ d y = (sum (getAllIncomeByYear y (cleanData d)) - sum (getAllExpenseByYear y (cleanData d)) ) / (fromIntegral (Prelude.length  (getAllExpenseByYear y (cleanData d))))
+
+getMaxBalance :: Int -> Int -> IO ()
+getMaxBalance m y = do
+  json <- (eitherDecode <$> getJSON) :: IO (Either String [Transaction])
+  case json of
+    Left err -> putStrLn err
+    Right d -> print (getMax (filterByMonthYear m y (cleanData d)))
+
+getMinBalance :: Int -> Int -> IO ()
+getMinBalance m y = do
+  json <- (eitherDecode <$> getJSON) :: IO (Either String [Transaction])
+  case json of
+    Left err -> putStrLn err
+    Right d -> print (getMin (filterByMonthYear m y (cleanData d)))
+
+getMeanIncomeByYear :: Int -> IO ()
+getMeanIncomeByYear y = do
+  json <- (eitherDecode <$> getJSON) :: IO (Either String [Transaction])
+  case json of
+    Left err -> putStrLn err
+    Right d -> print (getMeanIncomeByYear_ d y)
+
+getMeanIncomeByYear_ d y = (sum  (getAllIncomeByYear y (cleanData d))) / (fromIntegral (Prelude.length  (getAllIncomeByYear y (cleanData d))))
+
+getMeanExpenseByYear :: Int -> IO ()
+getMeanExpenseByYear y = do
+  json <- (eitherDecode <$> getJSON) :: IO (Either String [Transaction])
+  case json of
+    Left err -> putStrLn err
+    Right d -> print (getMeanExpenseByYear_ d y)
+
+getMeanExpenseByYear_ d y = (sum  (getAllExpenseByYear y (cleanData d))) / (fromIntegral (Prelude.length  (getAllExpenseByYear y (cleanData d))))
+
+getFlux :: Int -> Int -> IO ()
+getFlux m y = do
+  json <- (eitherDecode <$> getJSON) :: IO (Either String [Transaction])
+  case json of
+    Left err -> putStrLn err
+    Right d -> print (flux (filterByMonthYear m y (cleanData d)))
+
 main :: IO ()
 main = hspec $ do
 
   context "Projeto Funcional" $ do
-  describe "filterByYear" $ do
-  it "quantidade de transacoes no ano de 2017" $ do
-    d <- (eitherDecode <$> getJSON) :: IO (Either String [Crud.Transaction])
-    case d of
-        Left err -> putStrLn err
-        Right ps -> Prelude.length [x | x <- ps, (year (data_ x)) == 2017 ]  `shouldBe` 1336
-  it "quantidade de transacoes no ano de 2018" $ do
-    d <- (eitherDecode <$> getJSON) :: IO (Either String [Crud.Transaction])
-    case d of
-        Left err -> putStrLn err
-        Right ps -> Prelude.length [x | x <- ps, (year (data_ x)) == 2018 ]  `shouldBe` 1146
-  it "quantidade de transacoes no ano de 2019" $ do
-    d <- (eitherDecode <$> getJSON) :: IO (Either String [Crud.Transaction])
-    case d of
-        Left err -> putStrLn err
-        Right ps -> Prelude.length [x | x <- ps, (year (data_ x)) == 2019 ]  `shouldBe` 1207
 
-  describe "filterByYearMonth" $ do
-  it "quantidade de transacoes no ano de 2018 no mes 3" $ do
-    d <- (eitherDecode <$> getJSON) :: IO (Either String [Crud.Transaction])
-    case d of
+  it "transacoes em 2017 = 1336" $ do
+    json <- (eitherDecode <$> getJSON) :: IO (Either String [Transaction])
+    case json of
+        Left err -> putStrLn err
+        Right d -> Prelude.length (filterByYear_ d 2017) `shouldBe` 1336
+  it "transacoes em 2018 = 1146" $ do
+    json <- (eitherDecode <$> getJSON) :: IO (Either String [Transaction])
+    case json of
+        Left err -> putStrLn err
+        Right d -> Prelude.length (filterByYear_ d 2018)  `shouldBe` 1146
+  it "transacoes em 2019 = 1207" $ do
+    json <- (eitherDecode <$> getJSON) :: IO (Either String [Transaction])
+    case json of
+        Left err -> putStrLn err
+        Right d -> Prelude.length (filterByYear_ d 2019)  `shouldBe` 1207
+
+  it "transacoes em março de 2018 = 96" $ do
+    json <- (eitherDecode <$> getJSON) :: IO (Either String [Transaction])
+    case json of
       Left err -> putStrLn err
-      Right ps -> Prelude.length (filterByMonthYear 3 2018 ps) `shouldBe` 96
+      Right d -> Prelude.length (filterByMonthYear 3 2018 d) `shouldBe` 96
       
-  it "quantidade de transacoes no ano de 2019 no mes 1" $ do
-    d <- (eitherDecode <$> getJSON) :: IO (Either String [Crud.Transaction])
-    case d of
+  it "transacoes em janeiro de 2019 = 84" $ do
+    json <- (eitherDecode <$> getJSON) :: IO (Either String [Transaction])
+    case json of
       Left err -> putStrLn err
-      Right ps -> Prelude.length (filterByMonthYear 1 2019 ps) `shouldBe` 84
+      Right d -> Prelude.length (filterByMonthYear 1 2019 d) `shouldBe` 84
 
-  it "quantidade de transacoes no ano de 2018 no mes 12" $ do
-    d <- (eitherDecode <$> getJSON) :: IO (Either String [Crud.Transaction])
-    case d of
+  it "caso de erro: transacoes no mes 12 (inexistente) = 0" $ do
+    json <- (eitherDecode <$> getJSON) :: IO (Either String [Transaction])
+    case json of
       Left err -> putStrLn err
-      Right ps -> Prelude.length (filterByMonthYear 12 2018 ps) `shouldBe` 0
+      Right d -> Prelude.length (filterByMonthYear 12 2018 d) `shouldBe` 0
   
-  describe "getIncome" $ do        
-  it "Calcular o valor das receitas (créditos) no ano de 2018 no mes 12" $ do
-    d <- (eitherDecode <$> getJSON) :: IO (Either String [Crud.Transaction])
-    case d of
+  it "caso de erro: receitas no mes 12 (inexistente) = 0" $ do
+    json <- (eitherDecode <$> getJSON) :: IO (Either String [Transaction])
+    case json of
       Left err -> putStrLn err
-      Right ps -> (sum (getAllIncome 12 2018 (cleanData ps))) `shouldBe` 0.0
-  it "Calcular o valor das receitas (créditos) no ano de 2018 no mes 3" $ do
-    d <- (eitherDecode <$> getJSON) :: IO (Either String [Crud.Transaction])
-    case d of
-      Left err -> putStrLn err
-      Right ps -> (sum (getAllIncome 3 2018 (cleanData ps))) `shouldBe` 107375.22
+      Right d -> (sum (getAllIncome 12 2018 (cleanData d))) `shouldBe` 0.0
 
-  describe "getExpense" $ do        
-  it "Calcular o valor das despesas (débitos) ano de 2018 no mes 12" $ do
-    d <- (eitherDecode <$> getJSON) :: IO (Either String [Crud.Transaction])
-    case d of
+  it "receitas em janeiro de 2019 = 112735.66" $ do
+    json <- (eitherDecode <$> getJSON) :: IO (Either String [Transaction])
+    case json of
       Left err -> putStrLn err
-      Right ps -> (sum (getAllExpense 12 2018 (cleanData ps))) `shouldBe` 0.0
-  it "Calcular o valor das despesas (débitos) ano de 2018 no mes 3" $ do
-    d <- (eitherDecode <$> getJSON) :: IO (Either String [Crud.Transaction])
-    case d of
-      Left err -> putStrLn err
-      Right ps -> (sum (getAllExpense 3 2018 (cleanData ps))) `shouldBe` -107956.09
+      Right d -> (sum (getAllIncome 1 2019 (cleanData d))) `shouldBe` 112735.66
 
-  describe "calculaValorDasSobras" $ do        
-  it "Calcular a sobra (receitas - despesas) no ano de 2018 no mes 12" $ do
-    d <- (eitherDecode <$> getJSON) :: IO (Either String [Crud.Transaction])
-    case d of
+  it "caso de erro: depesas no mes 12 (inexistente) = 0" $ do
+    json <- (eitherDecode <$> getJSON) :: IO (Either String [Transaction])
+    case json of
       Left err -> putStrLn err
-      Right ps -> (sum (getAllIncome 12 2018 (cleanData ps)) - sum (getAllExpense 12 2018 (cleanData ps)) ) `shouldBe` 0.0
+      Right d -> (sum (getAllExpense 12 2018 (cleanData d))) `shouldBe` 0.0
 
-  it "Calcular a sobra (receitas - despesas) no ano de 2018 no mes 3" $ do
-    d <- (eitherDecode <$> getJSON) :: IO (Either String [Crud.Transaction])
-    case d of
+  it "despesas em janeiro de 2019 = -101597.95" $ do
+    json <- (eitherDecode <$> getJSON) :: IO (Either String [Transaction])
+    case json of
       Left err -> putStrLn err
-      Right ps -> (sum (getAllIncome 3 2018 (cleanData ps)) - sum (getAllExpense 3 2018 (cleanData ps)) ) `shouldBe` 215331.31
-    
-  describe "calculaSaldo" $ do        
-  it "Calcular o saldo final" $ do
-    d <- (eitherDecode <$> getJSON) :: IO (Either String [Crud.Transaction])
-    pending
+      Right d -> (sum (getAllExpense 1 2019 (cleanData d))) `shouldBe` -101597.95
+
+  it "caso de erro: sobra no mes 12 (inexistente) = 0" $ do
+    json <- (eitherDecode <$> getJSON) :: IO (Either String [Transaction])
+    case json of
+      Left err -> putStrLn err
+      Right d -> (getLeftOver_ d 12 2018) `shouldBe` 0.0
+
+  it "sobras em janeiro de 2019 = 214333.61" $ do
+    json <- (eitherDecode <$> getJSON) :: IO (Either String [Transaction])
+    case json of
+      Left err -> putStrLn err
+      Right d -> (getLeftOver_ d 1 2019) `shouldBe` 214333.61
   
-  describe "calculaSaldoMaximo" $ do        
-  it "Calcular o saldo máximo atingido no ano de 2018 no mes 3" $ do
-    d <- (eitherDecode <$> getJSON) :: IO (Either String [Crud.Transaction])
-    case d of
+  it "transacao com saldo maximo em janeiro de 2019 = -101597.95" $ do
+    json <- (eitherDecode <$> getJSON) :: IO (Either String [Transaction])
+    case json of
       Left err -> putStrLn err
-      Right ps -> (getId (getMax (filterByMonthYear 3 2018 (cleanData ps)))) `shouldBe` "CR\201D.LIQ.COBRAN\199A"
+      Right d -> (getId (getMax (filterByMonthYear 1 2019 (cleanData d)))) `shouldBe` "CR\201D.LIQ.COBRAN\199A"
     
-  describe "calculaSaldoMinimo" $ do        
-  it "Calcular o saldo minimo atingido no ano de 2018 no mes 3" $ do
-    d <- (eitherDecode <$> getJSON) :: IO (Either String [Crud.Transaction])
-    case d of
+  it "transacao com saldo minimo em janeiro de 2019 = -101597.95" $ do
+    json <- (eitherDecode <$> getJSON) :: IO (Either String [Transaction])
+    case json of
       Left err -> putStrLn err
-      Right ps -> (getId (getMin (filterByMonthYear 3 2018 (cleanData ps)))) `shouldBe` "CR\201D.LIQ.COBRAN\199A"
+      Right d -> (getId (getMin (filterByMonthYear 1 2019 (cleanData d)))) `shouldBe` "CR\201D.LIQ.COBRAN\199A"
   
-  describe "calculaMediaReceitas" $ do        
-  it "Calcular a média das receitas no ano de 2018" $ do
-    d <- (eitherDecode <$> getJSON) :: IO (Either String [Crud.Transaction])
-    case d of
+  it "média das receitas em 2019 = 6655.4834" $ do
+    json <- (eitherDecode <$> getJSON) :: IO (Either String [Transaction])
+    case json of
       Left err -> putStrLn err
-      Right ps -> ((sum  (getAllIncomeByYear 2018 (cleanData ps))) / (fromIntegral (Prelude.length  (getAllIncomeByYear 2018 (cleanData ps))))) `shouldBe` 5415.0576
+      Right d -> (getMeanIncomeByYear_ d 2019) `shouldBe` 6655.4834
     
-  describe "calculaMediaDespesas" $ do        
-  it "Calcular a média das despesas no ano de 2018" $ do
-    d <- (eitherDecode <$> getJSON) :: IO (Either String [Crud.Transaction])
-    case d of
+  it "média das despesas em 2019 = -1183.2368" $ do
+    json <- (eitherDecode <$> getJSON) :: IO (Either String [Transaction])
+    case json of
       Left err -> putStrLn err
-      Right ps -> ((sum  (getAllExpenseByYear 2018 (cleanData ps))) / (fromIntegral (Prelude.length  (getAllExpenseByYear 2018 (cleanData ps))))) `shouldBe` -1428.7981
+      Right d -> (getMeanExpenseByYear_ d 2019) `shouldBe` -1183.2368
     
-  describe "calculaMediaSobras" $ do        
-  it "Calcular a média das sobras no ano de 2018" $ do
-    d <- (eitherDecode <$> getJSON) :: IO (Either String [Crud.Transaction])
-    case d of
+  it "média das sobras em 2019 = 2484.6636" $ do
+    json <- (eitherDecode <$> getJSON) :: IO (Either String [Transaction])
+    case json of
       Left err -> putStrLn err
-      Right ps -> ((sum (getAllIncomeByYear 2018 (cleanData ps)) - sum (getAllExpenseByYear 2018 (cleanData ps)) ) / (fromIntegral (Prelude.length  (getAllExpenseByYear 2018 (cleanData ps))))) `shouldBe` 2957.3914
+      Right d -> ((sum (getAllIncomeByYear 2019 (cleanData d)) - sum (getAllExpenseByYear 2019 (cleanData d)) ) / (fromIntegral (Prelude.length  (getAllExpenseByYear 2019 (cleanData d))))) `shouldBe` 2484.6636
     
-  describe "retornaFluxoDeCaixa" $ do        
-  it "Retornar o flux de caixa de  no ano de 2018 no mes 3" $ do
-    d <- (eitherDecode <$> getJSON) :: IO (Either String [Crud.Transaction])
-    case d of
+  it "fluxo de caixa em janeiro de 2019" $ do
+    json <- (eitherDecode <$> getJSON) :: IO (Either String [Transaction])
+    case json of
       Left err -> putStrLn err
-      Right ps -> (flux (filterByMonthYear 3 2018 (cleanData ps))) `shouldBe` [(2,8.350048),(3,5962.22),(4,-789.92),(5,-23551.18),(6,-15569.9),(9,-1154.0),(10,-13446.461),(11,3908.8801),(12,1360.1399),(13,2607.11),(16,-6976.09),(17,2136.5999),(18,3174.2002),(19,-3577.6802),(20,55066.8),(23,1644.8301),(24,1905.19),(25,88.51997),(26,-14158.721),(27,475.88),(30,304.35)]
+      Right d -> (flux (filterByMonthYear 1 2019 (cleanData d))) `shouldBe` [(1,-21178.709),(4,1321.61),(5,3167.62),(6,-27167.23),(7,-12537.34),(8,-2172.18),(11,-6449.2207),(13,398.0),(14,1404.72),(15,667.8699),(18,7543.6),(19,9770.52),(20,62162.03),(21,-559.15),(22,-43.599976),(25,-1549.31),(26,443.84),(27,-3232.3901),(28,-853.0)]
     
     
 
